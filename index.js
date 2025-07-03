e86d6c0b748575006cfa874ef18a4b83fc9b91c2
TranTamNhu <tamnhu2722005@gmail.com>
2025-07-03 19:29:08 +0700

    Fix: corrected app.listen parameters for Docker deployment

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("*", (req, res) => {
 });
 app.use(errorHandler);
 
-app.listen(PORT,  () => {
+app.listen(PORT,'0.0.0.0',  () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
